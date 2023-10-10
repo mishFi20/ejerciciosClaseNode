0277ddfe70cd144859677d0e3b9e2902460290ee
mishFi20 <dsljdlls>
2023-10-10 16:04:33 -0600

    ejercicio de bombilla

---
+++ bombilla/codigo.js
@@ -0,0 +1,9 @@
+const cabeza = document.getElementById('cabeza');
+
+function encender (){
+    cabeza.style.backgroundColor = "yellow";
+}
+function apagar(){
+    cabeza.style.backgroundColor = "white";
+}
+
